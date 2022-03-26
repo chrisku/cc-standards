45085d52647dc15579e2dc487052ae928767e818
Christian Kuffner <ck@cypresscreative.com>
2022-03-26 18:21:24 -0500

    Plugin reset

---
+++ README.md
@@ -0,0 +1,4 @@
+# cc-standards.php
+
+This is a standard plugin we use to set up basic WordPress site configuration.
+
--- cc-standards.php
+++ cc-standards.php
@@ -76,35 +76,17 @@ add_filter( 'admin_email_check_interval', '__return_false' );
  * Plugin Name: Cypress Creative Standards
  * Plugin URI: https://cypr.es
  * Description: WordPress Website standard settings for Cypress Creative customers
- * Version: 1.002
+ * Version: 1.1
  * Text Domain: cc-standards
  * Author: Cypress Creative
  * Author URI: https://cypr.es
  */
 
-/**
-*Plugin update settings
-**/
-if( ! class_exists( 'cc_standards_Updater' ) ){
-	include_once(  'updater.php' );
-}
-
-$updater = new cc_standards_Updater( __FILE__ );
-$updater->set_username( 'chrisku');
-$updater->set_repository( 'cc-standards' );
-/*
-	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
-*/
-$updater->initialize();
+require_once('inc/updater.php')
 
 /**
  * Auto-Update Settings
  */
-
-// Enable/Disable the following as needed 
-// add_filter( 'auto_update_theme', '__return_true' );
-// add_filter( 'auto_update_plugin', '__return_true' );
-// add_filter( 'auto_update_translation', '__return_true' );
 
 // Disable auto-update email notifications for plugins.
 add_filter( 'auto_plugin_update_send_email', '__return_false' );
@@ -127,10 +109,7 @@ $current_time = current_datetime()->format( 'Hi' );
 $time_range = ( $current_time > 2200 || $current_time < 0600 );
 define( 'AUTOMATIC_UPDATER_DISABLED', $time_range );
 
-// WP Core - make sure to add this to wp-config.php 
-// define( 'WP_AUTO_UPDATE_CORE', true );
-// use this to only allow minor updates define( 'WP_AUTO_UPDATE_CORE', 'minor' );
-
+// Core Updates
 add_filter( 'allow_dev_auto_core_updates', '__return_false' ); // disables nightly builds
 add_filter( 'allow_minor_auto_core_updates', '__return_true' );
 add_filter( 'allow_major_auto_core_updates', '__return_true' );
@@ -138,9 +117,7 @@ add_filter( 'allow_major_auto_core_updates', '__return_true' );
 /**
  * Remove Dashboard Notifications
  */
- 
-// remove plugin notifications for non admins
- 
+
 // disable periodic admin email verification prompt
 add_filter( 'admin_email_check_interval', '__return_false' );
 >>>>>>> 26cc4ce (First commit)
